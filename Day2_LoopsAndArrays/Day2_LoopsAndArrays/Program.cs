c19c04510b10c5bc86e14dd34b42f24789f1f8e1
Techbear <techbearHub@users.noreply.github.com>
2020-08-31 21:02:00 +0300

    tasks 1 & 2

--- Day2_LoopsAndArrays/Day2_LoopsAndArrays/Program.cs
+++ Day2_LoopsAndArrays/Day2_LoopsAndArrays/Program.cs
@@ -6,7 +6,67 @@ namespace Day2_LoopsAndArrays
     {
         static void Main(string[] args)
         {
-            
+            Task2();
+        }
+
+        static void Task2()
+        {
+            String result = "";
+
+            for(int i = 0; i < 4; i++)
+            {
+                result += "*";
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4-i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Task1()
+        {
+            Random rand = new Random();
+            int biggest = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                int randNumber = rand.Next(60);
+
+                if(randNumber > biggest)
+                {
+                    biggest = randNumber;
+                }
+            }
+
+            Console.WriteLine(biggest);
+        }
+
+
+        static void Task1Extra()
+        {
+            Console.WriteLine("ievadiet skaitli!");
+            int biggest = Convert.ToInt32(Console.ReadLine()); ;
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("ievadiet skaitli!");
+                int randNumber = Convert.ToInt32(Console.ReadLine());
+
+                if (randNumber > biggest)
+                {
+                    biggest = randNumber;
+                }
+            }
+
+            Console.WriteLine(biggest);
         }
 
         static void RandomSample()
