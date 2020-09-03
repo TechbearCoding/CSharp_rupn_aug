99330d0a93aa533385f18054bee7441ee6caebe0
Techbear <techbearHub@users.noreply.github.com>
2020-09-03 19:30:00 +0300

    day 4 project created

---
+++ Day4_classes/Day4_classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day4_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
