b8844ec10503c5e5a6bace228038cbf35fbe9a7b
Techbear <techbearHub@users.noreply.github.com>
2020-09-07 20:18:03 +0300

    Čar class added

---
+++ Day6_Objects/Day6_Objects/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_Objects
+{
+    class Car
+    {
+        public String brand;
+        public String model;
+        public String color;
+
+        public void Drive()
+        {
+            Console.WriteLine("Brum brum");
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(color + " " + brand + " " + model);
+        }
+    }
+}
--- Day6_Objects/Day6_Objects/Program.cs
+++ Day6_Objects/Day6_Objects/Program.cs
@@ -6,7 +6,20 @@ namespace Day6_Objects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car car1 = new Car();
+            car1.color = "Red";
+            car1.brand = "Volkswagen";
+            car1.model = "Jetta";
+
+            Car car2 = new Car();
+            car2.color = "White";
+            car2.brand = "BMW";
+            car2.model = "M3";
+
+            car1.PrintInfo();
+
+            car2.PrintInfo();
+
         }
     }
 }
