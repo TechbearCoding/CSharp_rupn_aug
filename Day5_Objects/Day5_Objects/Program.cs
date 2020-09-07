2911f87bae8ffc82062256c74eaf55bbb75f8c3a
Techbear <techbearHub@users.noreply.github.com>
2020-09-07 18:56:23 +0300

    homework added

--- Day5_Objects/Day5_Objects/Program.cs
+++ Day5_Objects/Day5_Objects/Program.cs
@@ -7,7 +7,64 @@ namespace Day5_Objects
     {
         static void Main(string[] args)
         {
-            ListSample();
+            Task2();
+        }
+
+
+        static void Task2()
+        {
+            List<String> lst = new List<String>();
+
+            String choice = "";
+
+            while (choice != "0")
+            {
+                Console.WriteLine("1- Izvadit");
+                Console.WriteLine("2- Pievienot");
+                Console.WriteLine("0- Iziet");
+
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        PrintLst(lst);
+                        break;
+                    case "2":
+                        AddElement(lst);
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Nepareiza ievade");
+                        break;
+                }
+            }
+        }
+
+        private static void AddElement(List<string> lst)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Ievadiet elementu!");
+            lst.Add(Console.ReadLine());
+            Console.WriteLine();
+        }
+
+        private static void PrintLst(List<string> lst)
+        {
+            Console.WriteLine();
+
+            if(lst.Count == 0)
+            {
+                Console.WriteLine("Saraksts ir tukss!");
+                return;
+            }
+
+            for(int i = 0; i < lst.Count; i++)
+            {
+                Console.WriteLine(i + ": " + lst[i]);
+            }
+            Console.WriteLine();
         }
 
         static void ListSample()
