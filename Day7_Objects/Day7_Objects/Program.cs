c41a4b6e9ab4d6e1f8d6fdc528be4772fdea3b2a
Techbear <techbearHub@users.noreply.github.com>
2020-09-08 20:00:10 +0300

    student sample

--- Day7_Objects/Day7_Objects/Program.cs
+++ Day7_Objects/Day7_Objects/Program.cs
@@ -6,9 +6,26 @@ namespace Day7_Objects
     {
         static void Main(string[] args)
         {
-            Sample1();
+            Sample2();
         }
-        
+
+        static void Sample2()
+        {
+            Console.WriteLine("Ievadiet vardu!");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Ievadiet uzvardu!");
+            String lastName = Console.ReadLine();
+
+            Console.WriteLine("Ievadiet kursu!");
+            int course = Convert.ToInt32(Console.ReadLine());
+
+            Student st = new Student(name, lastName, course);
+
+            st.PrintInfo();
+        }
+
+
         static void Sample1()
         {
             Car car1 = new Car("BMW", "M3", "White");
---
+++ Day7_Objects/Day7_Objects/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7_Objects
+{
+    class Student
+    {
+        public String Name { get; set; }
+        public String LastName { get; set; }
+
+        private int _course;
+
+        public int Course 
+        {
+            get 
+            {
+                return _course;
+            }
+            set 
+            {
+                if(value >= 1 && value <= 3)
+                {
+                    _course = value;
+                }
+                else if(value > 3)
+                {
+                    _course = 3;
+                }
+                else
+                {
+                    _course = 1;
+                }
+            }
+
+        }
+
+        public Student(String name, String lastName, int course)
+        {
+            Name = name;
+            LastName = lastName;
+            Course = course;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(Name + " " + LastName + " " + Course);
+        }
+    }
+}
