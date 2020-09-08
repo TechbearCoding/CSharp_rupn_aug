b68af6ff16447c1afb28262d685574c9f893632d
Techbear <techbearHub@users.noreply.github.com>
2020-09-08 19:19:44 +0300

    car class

---
+++ Day7_Objects/Day7_Objects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day7_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample1();
+        }
+        
+        static void Sample1()
+        {
+            Car car1 = new Car("BMW", "M3", "White");
+            car1.PrintInfo();
+
+            Car c2 = new Car();
+            c2.PrintInfo();
+        }
+    }
+}
