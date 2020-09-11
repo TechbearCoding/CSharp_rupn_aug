7ea110a44f6ff41c29458a6e5fd5a44cb60e04dd
Techbear <techbearHub@users.noreply.github.com>
2020-09-11 19:54:00 +0300

    day 9

---
+++ Day9_Files/Day9_Files/FileOperations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day8_FileIO
+{
+    class FileOperations
+    {   
+        private const String defaultPath  = @"C:\Users\Marti\Desktop\";
+        public static void Read(String filename)
+        {
+            try
+            {
+                
+                StreamReader sr = new StreamReader(defaultPath + filename);
+
+                String line = sr.ReadLine();
+
+                while(line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas nolasit failu!");
+            }
+        }
+        public static void Write()
+        {
+
+            try
+            {
+                StreamWriter sw = new StreamWriter(defaultPath + "Test.txt", true);
+                sw.WriteLine("Hello ");
+                sw.WriteLine("world!");
+
+                sw.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas ierakstit faila!");
+            }
+        }
+    }
+}
---
+++ Day9_Files/Day9_Files/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day9_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
